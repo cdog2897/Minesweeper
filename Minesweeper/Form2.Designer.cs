fe36ddc77714b3a55be0e4cbf83f94e710917d4d
cdog2897 <caleb.wolin@gmail.com>
2022-10-31 15:15:17 -0700

    Add project files.

---
+++ Minesweeper/Form2.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Minesweeper
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Location = new System.Drawing.Point(32, 31);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(799, 719);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 871);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+    }
+}
---
+++ Minesweeper/Form2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form2 : Form
+    {
+        static int size_x = 13;
+        static int size_y = 9;
+        static Form2 frm = new Form2();
+        // grid of buttons
+        public Button[,] btnGrid = new Button[size_x, size_y];
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+            int buttonSize = mainPanel.Width / size_x;
+            mainPanel.Height = mainPanel.Width;
+
+            for (int x = 0; x < size_x; x++)
+            {
+                for (int y = 0; y < size_y; y++)
+                {
+                    btnGrid[x, y] = new Button();
+                    btnGrid[x, y].Width = buttonSize;
+                    btnGrid[x, y].Height = buttonSize;
+
+                    btnGrid[x, y].MouseUp += Grid_Button_Click;
+
+                    mainPanel.Controls.Add(btnGrid[x, y]);
+                    btnGrid[x, y].Location = new Point(x * buttonSize, y * buttonSize);
+                    //btnGrid[x, y].Text = x + "|" + y;
+                    btnGrid[x, y].Text = "0";
+
+                    btnGrid[x, y].Tag = new Point(x, y);
+                }
+            }
+        }
+
+        private void Grid_Button_Click(object sender, MouseEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            Point location = (Point)clickedButton.Tag;
+
+            int x = location.X;
+            int y = location.Y;
+
+            int num = Int32.Parse(btnGrid[x, y].Text);
+            if(e.Button == MouseButtons.Left)
+            {
+                num++;
+                btnGrid[x, y].Text = num.ToString();
+            }
+            else if(e.Button == MouseButtons.Right)
+            {
+                num--;
+                btnGrid[x, y].Text = num.ToString();
+            }
+
+        }
+    }
+}
